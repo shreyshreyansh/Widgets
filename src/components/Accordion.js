ae80b98e80776ff83811f88aaeb37ccb2fa87bec
Shreyansh Shrey <shreyansh.shrey1999@gmail.com>
2020-10-20 10:11:05 +0530

    Add useState to accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Accordion({ items }) {
 
+    const [activeIndex, setActiveIndex] = useState(null);
+
     const onTitleClick = (index) => {
-        console.log('title clicked', index);
+        setActiveIndex(index);
     }
 
     const renderedItems = items.map((item, index) => {
@@ -23,7 +25,10 @@ function Accordion({ items }) {
         );
     });
 
-    return <div className="ui styled accordion">{renderedItems}</div>
+    return <div className="ui styled accordion">
+                {renderedItems}
+                <h1>{activeIndex}</h1>
+            </div>
 }
 
 export default Accordion
